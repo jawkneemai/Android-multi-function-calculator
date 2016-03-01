2b831b35132b9504b79e88064011aa3bfc6bc389
Johnny <johnathm@usc.edu>
2016-02-29 23:25:23 -0800

    Finished unit conversion and currency exchange functionalitygit add * -f added error checks

--- app/src/main/java/itp341/mai/johnathan/a5/FragmentMoneyExchange.java
+++ app/src/main/java/itp341/mai/johnathan/a5/FragmentMoneyExchange.java
@@ -2,23 +2,43 @@ package itp341.mai.johnathan.a5;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-/**
- * Created by BabyJ on 2/28/16.
- */
+import java.text.DecimalFormat;
+
 public class FragmentMoneyExchange extends Fragment {
 
-    public FragmentMoneyExchange() {
+    EditText mEditTextInput;
+    Spinner mSpinnerFrom;
+    Spinner mSpinnerTo;
+    Button mButtonConvert;
+    TextView mTextViewResult;
 
+    final DecimalFormat df = new DecimalFormat("0.00");
+    double exchangeAmount = 0.0;
+    double resultAmount = 0.0;
+
+    // Multiplier Arrays
+    double[] USDMultipliers = { 1.0, 6.51, 0.90 };
+    double[] yuanMultipliers = { 0.15, 1.0, 0.14 };
+    double[] euroMultipliers = {1.12, 7.27, 1.0 };
+
+
+
+    public FragmentMoneyExchange() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -26,6 +46,44 @@ public class FragmentMoneyExchange extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_money_exchange, container, false);
+
+        mEditTextInput = (EditText) v.findViewById(R.id.editTextMoneyExchange);
+        mSpinnerFrom = (Spinner) v.findViewById(R.id.spinnerMoneyFrom);
+        mSpinnerTo = (Spinner) v.findViewById(R.id.spinnerMoneyTo);
+        mButtonConvert = (Button) v.findViewById(R.id.buttonConvertMoney);
+        mTextViewResult = (TextView) v.findViewById(R.id.textViewResultMoney);
+
+        mButtonConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(mEditTextInput.getText().toString())) {
+                    mEditTextInput.setError(getResources().getString(R.string.empty_amount_error));
+                } else {
+                    exchangeAmount = Double.parseDouble(mEditTextInput.getText().toString());
+                    switch (mSpinnerFrom.getSelectedItemPosition()) {
+                        case 0:
+                            mTextViewResult.setVisibility(View.VISIBLE);
+                            resultAmount = exchangeAmount * USDMultipliers[mSpinnerTo.getSelectedItemPosition()];
+                            mTextViewResult.setText(String.valueOf(df.format(resultAmount)));
+                            break;
+                        case 1:
+                            mTextViewResult.setVisibility(View.VISIBLE);
+                            resultAmount = exchangeAmount * yuanMultipliers[mSpinnerTo.getSelectedItemPosition()];
+                            mTextViewResult.setText(String.valueOf(df.format(resultAmount)));
+                            break;
+                        case 2:
+                            mTextViewResult.setVisibility(View.VISIBLE);
+                            resultAmount = exchangeAmount * euroMultipliers[mSpinnerTo.getSelectedItemPosition()];
+                            mTextViewResult.setText(String.valueOf(df.format(resultAmount)));
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+        });
+
         return v;
     }
 }
--- app/src/main/java/itp341/mai/johnathan/a5/FragmentUnitConversion.java
+++ app/src/main/java/itp341/mai/johnathan/a5/FragmentUnitConversion.java
@@ -2,12 +2,39 @@ package itp341.mai.johnathan.a5;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class FragmentUnitConversion extends Fragment {
+
+    EditText mEditTextInput;
+    RadioGroup mRadioGroupFrom;
+    RadioGroup mRadioGroupTo;
+    Button mButtonConvert;
+    TextView mTextViewResult;
+
+    // Instance Variables
+    double amountConvert = 0.0;
+    double resultConvert = 0.0;
+
+    // Arrays for multipliers
+
+    double[] centimetersConvert = { 1.0, 0.01, 0.0328, 0.00000621, 0.00001 };
+    double[] metersConvert = { 100.0, 1.0, 3.2808, 0.000621, 0.01 };
+    double[] feetConvert = { 30.48, 0.3048, 1.0, 0.000189, 0.000304 };
+    double[] milesConvert = { 160934.0, 1609.34, 5280.0, 1.0, 1.60934 };
+    double[] kilometersConvert = { 10000.0, 1000.0, 3280.84, 0.62137, 1.0 };
+
+    private static final String TAG = FragmentUnitConversion.class.getSimpleName();
+
 
     public FragmentUnitConversion() {
 
@@ -24,6 +51,72 @@ public class FragmentUnitConversion extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_unit_conversion, container, false);
+
+        mEditTextInput = (EditText) v.findViewById(R.id.editTextUnitConv);
+        mRadioGroupFrom = (RadioGroup) v.findViewById(R.id.radioGroupUnitConvFrom);
+        mRadioGroupTo = (RadioGroup) v.findViewById(R.id.radioGroupUnitConvTo);
+        mButtonConvert = (Button) v.findViewById(R.id.buttonConvertUnit);
+        mTextViewResult = (TextView) v.findViewById(R.id.textViewResultUnit);
+
+        mButtonConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(mEditTextInput.getText().toString())) {
+                    mEditTextInput.setError(getResources().getString(R.string.empty_amount_error));
+                } else {
+                    amountConvert = Double.parseDouble(mEditTextInput.getText().toString());
+                    switch (mRadioGroupFrom.getCheckedRadioButtonId()) {
+                        case -1:
+                            mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            break;
+                        case 1:
+                            if (mRadioGroupTo.getCheckedRadioButtonId() == -1) {
+                                mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            } else {
+                                resultConvert = amountConvert * centimetersConvert[mRadioGroupTo.getCheckedRadioButtonId() - 6];
+                                mTextViewResult.setText(String.valueOf(resultConvert));
+                            }
+                            break;
+                        case 2:
+                            if (mRadioGroupTo.getCheckedRadioButtonId() == -1) {
+                                mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            } else {
+                                resultConvert = amountConvert * metersConvert[mRadioGroupTo.getCheckedRadioButtonId() - 6];
+                                mTextViewResult.setText(String.valueOf(resultConvert));
+                            }
+                            break;
+                        case 3:
+                            if (mRadioGroupTo.getCheckedRadioButtonId() == -1) {
+                                mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            } else {
+                                resultConvert = amountConvert * feetConvert[mRadioGroupTo.getCheckedRadioButtonId() - 6];
+                                mTextViewResult.setText(String.valueOf(resultConvert));
+                            }
+                            break;
+                        case 4:
+                            if (mRadioGroupTo.getCheckedRadioButtonId() == -1) {
+                                mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            } else {
+                                resultConvert = amountConvert * milesConvert[mRadioGroupTo.getCheckedRadioButtonId() - 6];
+                                mTextViewResult.setText(String.valueOf(resultConvert));
+                            }
+                            break;
+                        case 5:
+                            if (mRadioGroupTo.getCheckedRadioButtonId() == -1) {
+                                mTextViewResult.setText(getResources().getString(R.string.error_radio_button));
+                            } else {
+                                resultConvert = amountConvert * kilometersConvert[mRadioGroupTo.getCheckedRadioButtonId() - 6];
+                                mTextViewResult.setText(String.valueOf(resultConvert));
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        });
+
         return v;
     }
 
